dc535075b53273a9cf46bddd588e0319adb3bad9
Hsiaoming Yang <me@lepture.com>
2015-06-14 00:19:09 +0800

    User view with head cover

--- lib/user.vue
+++ lib/user.vue
@@ -1,3 +1,43 @@
 <template>
-<h2>User view</h2>
+  <div class="header">
+    <div class="header-intro">
+      <div class="container">
+        <h2>{{ user.username }}</h2>
+        <p>{{ user.description }}</p>
+      </div>
+    </div>
+    <div class="header-nav">
+      <div class="container">
+        <nav>
+          <a href="/u/{{user.username}}/topics">Topics</a>
+          <a href="/u/{{user.username}}/likes">Likes</a>
+          <a href="/u/{{user.username}}/replies">Replies</a>
+          <a href="/u/{{user.username}}/cafes">Cafes</a>
+        </nav>
+      </div>
+    </div>
+  </div>
+
+  <div class="body">
+    <div class="split-view container">
+      <div class="main-view">
+        <topic-list topics="{{ topics }}"></topic-list>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+  module.exports = {
+    replace: true,
+    props: ['params'],
+    data: function() {
+      return {
+        user: {
+          username: 'lepture',
+          description: 'Love its people, but never trust its government.'
+        }
+      }
+    }
+  };
+</script>
