47a2c6ff773a5f78e4744cca0b271552a308fd0d
Hsiaoming Yang <me@lepture.com>
2015-06-26 16:47:37 +0800

    Add following and unfollow button on cafe header

--- lib/components/cafe-header.vue
+++ lib/components/cafe-header.vue
@@ -15,8 +15,12 @@
           <a href="/c/{{ cafe.slug }}/members">Members</a>
           <a v-if="cafe.intro" href="/t/{{ cafe.intro }}">Introduction</a>
         </nav>
-        <div class="header-actions">
-          <a class="button" href="#">Follow</a>
+        <div class="header-actions" v-if="cafe.id">
+          <button v-if="!following" v-on="click:follow">Follow</button>
+          <button class="unfollow-button" v-if="following" v-on="click:unfollow">
+            <span class="unfollow-fg">Following</span>
+            <span class="unfollow-bg">Unfollow</span>
+          </button>
         </div>
       </div>
     </div>
@@ -24,25 +28,52 @@
 </template>
 
 <script>
+var api = require('../api');
 module.exports = {
   replace: true,
   props: ['cafe'],
-  data: function() {
-    return {
-      cafe: {},
-      style: {}
-    };
+  computed: {
+    style: function() {
+      var style = this.cafe.style;
+      if (!style || !style.cover) return {};
+      return {'background-image': 'url(' + style.cover + ')'};
+    },
+    following: function() {
+      var role = this.cafe.membership;
+      if (!role) return false;
+      return role != 'visitor';
+    }
   },
-  watch: {
-    'cafe.style': function(style) {
-      if (!style) return;
-
-      var rv = {};
-      if (style.cover) {
-        rv['background-image'] = 'url(' + style.cover + ')';
-      }
-      this.style = rv;
+  methods: {
+    follow: function() {
+      api.cafe.join(this.cafe.slug, function() {
+        this.cafe.membership = 'subscriber';
+      }.bind(this));
+    },
+    unfollow: function() {
+      api.cafe.leave(this.cafe.slug, function() {
+        this.cafe.membership = 'visitor';
+      }.bind(this));
     }
   }
 }
 </script>
+
+<style>
+.unfollow-button {
+  width: 9.6em;
+}
+.unfollow-button .unfollow-bg {
+  display: none;
+}
+.unfollow-button:hover {
+  background-color: #FF4444;
+}
+.unfollow-button:hover .unfollow-bg {
+  display: inline;
+}
+.unfollow-button:hover .unfollow-fg {
+  display: none;
+}
+
+</style>
