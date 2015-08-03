0dd7f3fd5fa6e5619c58f7123c9b968223107fa8
Hsiaoming Yang <me@lepture.com>
2015-08-03 18:07:40 +0800

    Add fabfile for uploading

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+
+import os
+import glob
+import hashlib
+import datetime
+from fabric.api import local
+
+
+def upload(filename):
+    now = datetime.datetime.now()
+
+    with open(filename) as f:
+        hsh = hashlib.md5(f.read()).hexdigest()[:6]
+
+    name = '%s.%s' % (now.strftime('%Y%m%d'), hsh)
+    name = name + os.path.splitext(filename)[1]
+    local('qboxrsctl put -c python qingcheng/%s %s' % (name, filename))
+
+
+def assets():
+    upload('dist/qingcheng.js')
+    upload('dist/qingcheng.css')
+
+
+def fonts():
+    for name in glob.glob('dist/fonts/*'):
+        key = name.replace('dist', 'qingcheng')
+        local('qboxrsctl put -c python %s %s' % (key, name))
